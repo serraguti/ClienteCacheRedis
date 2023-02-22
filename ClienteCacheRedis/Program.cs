07e0798fdfbab609b24024c57aa2eee5e4208bfe
serraguti <pacoserranox@gmail.com>
2023-02-22 16:25:59 +0100

    Add project files.

---
+++ ClienteCacheRedis/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ClienteCacheRedis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cliente MOVIL de Cache Redis");
+            Console.WriteLine("--------------------");
+            ServiceCacheRedis service = new ServiceCacheRedis();
+            string fin = "y";
+            while (fin.ToLower() != "n")
+            {
+                List<Producto> favoritos = service.GetProductosFavoritos();
+                if (favoritos == null)
+                {
+                    Console.WriteLine("No existe favoritos");
+                }
+                else
+                {
+                    int i = 1;
+                    foreach (Producto prod in favoritos)
+                    {
+                        Console.WriteLine(i + ".- " + prod.Nombre);
+                    }
+                    Console.WriteLine("----------------");
+                }
+                Console.WriteLine("¿Desea seguir cargando favoritos? (y/n)");
+                fin = Console.ReadLine();
+            }
+            Console.WriteLine("Fin de programa");
+        }
+    }
+}
